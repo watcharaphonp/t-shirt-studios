5863e1e15df37b21eac7f7100c445bb43e16bfac
watcharaphonp <watcharaphon@treg.co.th>
2024-10-08 11:59:10 +0700

    block unauth user

--- app/routes/dashboard._index.tsx
+++ app/routes/dashboard._index.tsx
@@ -1,5 +1,12 @@
+import { useNavigate } from '@remix-run/react'
 import DashboardPage from '~/components/DashboardPage'
+import { useAuth } from '~/contexts/authContext'
 
 export default function DashboardLayout() {
+    const { user } = useAuth()
+    const navigate = useNavigate()
+
+    if (user === null) navigate('/login')
+
     return <DashboardPage />
 }
